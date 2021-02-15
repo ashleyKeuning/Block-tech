e3048efe0f0c8b767388c585fc549bf8efe330d4
Ashley Keuning <70710193+ashleyKeuning@users.noreply.github.com>
2021-02-15 12:39:09 +0100

    changes

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 console.log('hello world');
 
+//imports
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const port = 3000;
 
+//static files
 app.use(express.static('static/public'))
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render ('home');
 });
 
-app.get ('/recommendations', (req, res) => {
-res.send('todays recommendations')
+app.get('/recommendations', (req, res) => {
+  res.render ('recommendations');
 });
-
-app.get ('/recommendations/:recId', (req, res) => {
-  res.send(`<h1>detail of todays recommendation ${req.params.recId}</h1>`)
-  });
 
 app.use (function (req, res, next) {
     res.status (404).send("no, this page is challas! ... sorry!")
     });
   
-
+//listen
 app.listen(3000, () => {
   console.log('Express web app on localhost:3000');
 });
