0e7d5105211d025d54407f8e20aab1d45985c58c
Ashley Keuning <70710193+ashleyKeuning@users.noreply.github.com>
2021-02-17 13:44:29 +0100

    dynamic data uitgeprobeerd

--- index.js
+++ index.js
@@ -6,18 +6,27 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const port = 3000;
 
+//test
+const accounts = [
+  {name: "drake", age: "34", location: "4.5 km away"},
+  {name: "abel", age: "31", location: "5 km away"},
+  {name: "bryson", age: "28", location: "8 km away"}
+];
 //static files
 app.use(express.static('public'))
 
-app.engine('handlebars', exphbs());
+//dynamic data
 app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ 
+  defaultLayout: 'main',}));
+
 
 app.get('/', (req, res) => {
   res.render ('home');
 });
 
 app.get('/recommendations', (req, res) => {
-  res.render ('recommendations');
+  res.render ('recommendations',{title:"Een lijst met accounts", accounts});
 });
 
 app.use (function (req, res, next) {
